32ab232488d3a2bda792f702d9d3e817cbad61b6
Thomas Hertog <thomas.hertog@external.toyota-europe.com>
2025-08-13 10:42:00 +0200

    fix validation and error styling

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector('form').addEventListener('submit', function(event){
+document.querySelector('form').addEventListener('submit', function (event) {
     event.preventDefault();
     updateTextValidity();
     updateEmailValidity();
     updateRadioValidity();
-    if(event.target.checkValidity()) {
+    if (event.target.checkValidity()) {
         const dialog = document.getElementById('success-dialog');
         dialog.show();
         setTimeout(() => dialog.close(), 5000);
@@ -14,51 +14,69 @@ document.getElementById('email').addEventListener('change', updateEmailValidity)
 document.querySelectorAll('[role="radiogroup"] input[type="radio"]').forEach(radio => {
     radio.addEventListener('change', updateRadioValidity);
 });
+document.getElementById('first-name').addEventListener('change', updateFirstNameValidity);
+document.getElementById('last-name').addEventListener('change', updateLastNameValidity);
+document.getElementById('message').addEventListener('change', updateMessageValidity);
+
 
 function updateEmailValidity() {
     const emailInput = document.getElementById('email');
     const emailRequired = document.getElementById('email-required');
     const emailInvalid = document.getElementById('email-invalid');
 
-    if(!emailInput.validity.valid){
-        if(emailInput.validity.valueMissing) {
+    if (!emailInput.validity.valid) {
+        if (emailInput.validity.valueMissing) {
             emailRequired.classList.remove('sr-only');
             emailInvalid.classList.add('sr-only');
-        }else if(emailInput.validity.typeMismatch) {
+        } else if (emailInput.validity.typeMismatch) {
             emailInvalid.classList.remove('sr-only');
             emailRequired.classList.add('sr-only');
-        }            
+        }
     }
 }
 
 function updateRadioValidity() {
     const radiobuttons = document.querySelector('[role="radiogroup"]');
-    if(![...radiobuttons.elements].some(radio => radio.checked)) {
+    if (![...radiobuttons.elements].some(radio => radio.checked)) {
         radiobuttons.ariaInvalid = 'true';
         radiobuttons.validity.valueMissing = true;
         radiobuttons.validity.valid = false;
-    }else{
+    } else {
         radiobuttons.ariaInvalid = 'false';
         radiobuttons.validity.valueMissing = false;
         radiobuttons.validity.valid = true;
     }
 }
 
-function updateTextValidity(){
-    const firstName = document.getElementById('first-name');
-    const lastName = document.getElementById('last-name');
-    const message = document.getElementById('message');
+function updateTextValidity() {
+    updateFirstNameValidity();
+    updateLastNameValidity();
+    updateMessageValidity();
+}
 
-    if(firstName.value.trim() === ''){
+function updateFirstNameValidity() {
+    const firstName = document.getElementById('first-name');
+    if (firstName.value.trim() === '') {
         firstName.validity.valueMissing = true;
         firstName.validity.valid = false;
+        firstName.ariaInvalid = 'true';
     }
-    if(lastName.value.trim() === ''){
+}
+
+function updateLastNameValidity() {
+    const lastName = document.getElementById('last-name');
+    if (lastName.value.trim() === '') {
         lastName.validity.valueMissing = true;
         lastName.validity.valid = false;
+        lastName.ariaInvalid = 'true';
     }
-    if(message.value.trim() === ''){
+}
+
+function updateMessageValidity() {
+    const message = document.getElementById('message');
+    if (message.value.trim() === '') {
         message.validity.valueMissing = true;
         message.validity.valid = false;
+        message.ariaInvalid = 'true';
     }
 }
